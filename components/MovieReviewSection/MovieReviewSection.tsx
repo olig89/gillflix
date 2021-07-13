6b4c71f5903da0705b9aceb3f3b653d43a1efabb
Oli Gill <oliver.gill.89@gmail.com>
2021-07-13 19:58:30 +0300

    margins right way around this time

--- components/MovieReviewSection/MovieReviewSection.tsx
+++ components/MovieReviewSection/MovieReviewSection.tsx
@@ -41,13 +41,13 @@ const Review = ({ review }: { review: ReviewType<UserType> }) => {
             ml={{ base: 0, lg: 'auto' }}
             alignItems="center"
           >  
-              <CircularProgress value={review.concept} min={0} max={10} mr={5} mt={[1,1,0,0]} color="cyan.400" size="30px" thickness="14px">
+              <CircularProgress value={review.concept} min={0} max={10} mr={5} mt={[0,0,1,1]} color="cyan.400" size="30px" thickness="14px">
                 <CircularProgressLabel color={'gray.500'} fontWeight="semibold" fontSize="xs">{review.concept}</CircularProgressLabel>
               </CircularProgress>
-              <CircularProgress value={review.cinema} min={0} max={10} mr={5} mt={[1,1,0,0]} color="yellow.500" size="30px" thickness="14px">
+              <CircularProgress value={review.cinema} min={0} max={10} mr={5} mt={[0,0,1,1]} color="yellow.500" size="30px" thickness="14px">
                 <CircularProgressLabel color={'gray.500'} fontWeight="semibold" fontSize="xs">{review.cinema}</CircularProgressLabel>
               </CircularProgress>
-              <CircularProgress value={review.perform} min={0} max={10} mr={5} mt={[1,1,0,0]} color="red.400" size="30px" thickness="14px">
+              <CircularProgress value={review.perform} min={0} max={10} mr={5} mt={[0,0,1,1]} color="red.400" size="30px" thickness="14px">
                 <CircularProgressLabel color={'gray.500'} fontWeight="semibold" fontSize="xs">{review.perform}</CircularProgressLabel>
               </CircularProgress>
             <Text fontSize="4xl" fontWeight="bold">
