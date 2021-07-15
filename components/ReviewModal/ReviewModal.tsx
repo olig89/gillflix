fd606f0b25a3e46df8f6b87f7ea0471b266d54a4
Oli Gill <oliver.gill.89@gmail.com>
2021-07-16 02:55:42 +0300

    margin+pad test accordion

--- components/ReviewModal/ReviewModal.tsx
+++ components/ReviewModal/ReviewModal.tsx
@@ -216,7 +216,7 @@ export const ReviewModal: React.FC<{ isAdmin: boolean; inNav?: boolean }> = ({
                     <AccordionIcon />
                 </HStack>
             </AccordionButton>
-            <AccordionPanel ml={-3} pt={-4} pb={4}>
+            <AccordionPanel ml={-3} pt={-10} pb={4}>
               <FormLabel fontSize="1em">
                 Premise, plot, and structure
               </FormLabel>
@@ -275,7 +275,7 @@ export const ReviewModal: React.FC<{ isAdmin: boolean; inNav?: boolean }> = ({
                 </HStack>
               <AccordionIcon />
             </AccordionButton>
-            <AccordionPanel ml={-3} pb={4}>
+            <AccordionPanel ml={-3} mt={-10} pb={4}>
             <FormLabel fontSize="1em">
               Visuals, sound, direction, and design
             </FormLabel>
