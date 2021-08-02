507a30372757bf6d749b4c7f49a31db0db0ce09a
Oli Gill <oliver.gill.89@gmail.com>
2021-08-02 12:55:57 +0300

    attempt to mobile friendly it up

--- components/AboutUserSection/AboutUserSection.tsx
+++ components/AboutUserSection/AboutUserSection.tsx
@@ -20,16 +20,16 @@ export const AboutUserSection: React.FC<AboutUserSectionProps> = ({
 }): React.ReactElement => {
   return (
     <Flex justifyContent="center">
-      <Avatar mr={10} size="2xl" src={user.image} />
+      <Avatar mr={10} size="2xl" src={user.image} display={{base: 'none', md:'block'}} />
       <VStack textAlign="left" alignItems="flex-start">
         <Heading size="3xl">
           {user.username}
-          <chakra.span color="gray.500" fontSize="2xl">
+          <chakra.span color="gray.500" fontSize="2xl" display={{base: 'none', md:'block'}}>
             #{user.discriminator}
           </chakra.span>
         </Heading>
         <Text fontSize="2xl" color="gray.500" alignSelf="flex-start">
-          {reviews.length === 0 ? 'No reviews' : reviews.length + ' Rating'}
+          {reviews.length === 0 ? 'No reviews' : reviews.length + ' Review'}
           {reviews.length > 1 ? 's' : ''}{' '}
           {reviews.length > 0 &&
             '·  ' +
